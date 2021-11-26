566d81498ab18ceaad94f6d970891b9db6b11476
imgomez0127 <ianm0127@gmail.com>
2021-11-26 16:26:30 -0500

    Added login and signup route

--- backend/app.py
+++ backend/app.py
@@ -8,17 +8,14 @@ CORS(app)
 @app.route('/sign-up', methods=['POST'])
 @cross_origin()
 def sign_up():
-    print(request.json)
     # Intentionally left as bad practice
     username = request.json['username']
     password = request.json['password']
-    print(username, password)
     with open('db.json', 'r') as f:
         db = json.load(f)
     if username in db['users']:
         # As of flask V1.1.0 jsonify gets implicitly called on dict returns
-        return {"Error": "User already exists"}
-    db['id'] += 1
+        return {'Error': 'User already exists'}
     db['users'][username] = {
         'password': password,
         'id': db['id'],
@@ -29,9 +26,25 @@ def sign_up():
         'coffeeshops': [],
         'meetings': []
     }
+    db['id'] += 1
     with open('db.json', 'w') as f:
         json.dump(db, f)
-    return {"Status": "User Created"}
+    return {'User': username, 'id': db['id']}
+
+@app.route('/login', methods=['POST'])
+@cross_origin()
+def login():
+    # Intentionally left as bad practice
+    username = request.json['username']
+    password = request.json['password']
+    with open('db.json', 'r') as f:
+        db = json.load(f)
+    if username not in db['users']:
+        # As of flask V1.1.0 jsonify gets implicitly called on dict returns
+        return {'Error': 'Incorrect login credentials'}
+    if db['users']['usersname']['password'] != password:
+        return {'Error': 'Incorrect login credentials'}
+    return {'User': username, 'id': db['id']}
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=8000, debug=True)
