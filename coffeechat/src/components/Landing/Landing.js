94c3e2c096318adff3d6df3fea838e5390eada06
imgomez0127 <ianm0127@gmail.com>
2021-11-26 16:24:44 -0500

    Added login and error messages to login and sign up

--- coffeechat/src/components/Landing/Landing.js
+++ coffeechat/src/components/Landing/Landing.js
@@ -2,18 +2,46 @@ import photo from '../../photos/landing-header.png';
 import axios from 'axios';
 import '../../styles/homepage.css';
 import React from 'react';
-let signup_route = "http://127.0.0.1:8000/sign-up"
-let createUser = (username, password) => {
-  console.log("HELLO WORLD")
-  axios.post(signup_route,
+import {useNavigate} from 'react-router-dom'
+
+let backend_route = "http://127.0.0.1:8000/"
+
+let createUser = async (username, password, setError, navigate) => {
+  let signup_route = backend_route + 'sign-up';
+  let signup_result = await axios.post(signup_route,
              {
                username: username,
                password: password
              });
+
+  if (signup_result['Error']) {
+   setError(signup_result['Error']);
+  } else {
+    navigate('/preferences', { replace: true })
+  }
 }
+
+let login = async (username, password, setError, navigate) => {
+
+  let login_route = backend_route + 'login';
+  let login_result = await axios.post(login_route,
+             {
+               username: username,
+               password: password
+             });
+  if (login_result['Error']) {
+   setError(login_result['Error']);
+  } else {
+    navigate('/meetings', { replace: true })
+  }
+
+}
+
 function Landing() {
   const [username, setUsername] = React.useState(null);
   const [password, setPassword] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const navigate = useNavigate();
   return (
     <div className="Landing">
       <div className="background"> </div>
@@ -33,8 +61,9 @@ function Landing() {
             <input type='text' name='password' placeholder="password" onChange={e => setPassword(e.target.value)}/>
         </label>
         </div>
-        <input  type="submit" value="Submit" />
-    <a className="sign-up" href='/preferences' onClick={e => createUser(username, password)}>Sign Up</a>
+        <div className="sign-up" onClick={e => login(username, password, setError, navigate)}>Login</div>
+        <div className="sign-up" onClick={e => createUser(username, password, setError, navigate)}>Sign Up</div>
+        {(error) && <div><p> {error} </p> </div>}
       </form>
     </div>
     </div>
