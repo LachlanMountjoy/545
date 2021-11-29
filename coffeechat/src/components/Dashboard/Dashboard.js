f5a359a59266072579af628702c737072f082ca0
imgomez0127 <ianm0127@gmail.com>
2021-11-28 21:17:47 -0500

    Added meeting stuff

--- coffeechat/src/components/Dashboard/Dashboard.js
+++ coffeechat/src/components/Dashboard/Dashboard.js
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-let backendRoute = '127.0.0.1:8000/'
+let backend_route = "http://127.0.0.1:8000/";
+
 let getMeeting = async (setMeetings, username) => {
-    let meetings = await axios.get(backendRoute + `get-meetings/${username}`)['data']['meetings']
-    setMeetings(meetings)
+    console.log('backend call: ' + backend_route + `get-meetings/${username}`);
+    let meetings = await axios.get(backend_route + `get-meetings/${username}`);
+    console.log(meetings);
+    setMeetings(meetings['data']['meetings']);
 }
 
 function Dashboard({userObject}){
     console.log(userObject)
     let [meetings, setMeetings] = useState(null);
+    console.log(meetings)
     useEffect(() => {
-        if(meetings === null) {
+        if(meetings === null){
             getMeeting(setMeetings, userObject['username']);
         }
     }, [meetings, userObject])
-    return (
-        // Iterate over the userObject's meeting field to display all the meetings for a User
-        <div className="Meetings">
-            {meetings.map(meeting => <p>{meeting}</p>)}
-      </div>
-    );
+    if(meetings && meetings.length === 0){
+        return (
+            // Iterate over the userObject's meeting field to display all the meetings for a User
+            <div className="Meetings">
+
+            </div>
+        );
+    }
+    else{
+        return (<div><p>no meetings found</p></div>)
+    }
 }
 
 export default Dashboard;
