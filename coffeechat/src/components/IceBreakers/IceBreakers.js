df7f9f14c60fc81ad88e952c3677d030e0bafb8b
imgomez0127 <ianm0127@gmail.com>
2021-12-03 00:32:24 -0500

    Added icon

--- coffeechat/src/components/IceBreakers/IceBreakers.js
+++ coffeechat/src/components/IceBreakers/IceBreakers.js
@@ -1,10 +1,11 @@
 import {icebreakers} from '../../icebreakers.json';
 import NavBar from '../NavBar/NavBar.js';
-
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 
 
 function IceBreakers({userObject, setCookie}) {
@@ -14,7 +15,7 @@ function IceBreakers({userObject, setCookie}) {
         <NavBar setCookie={setCookie}/>
         {icebreakerTopics.map(topic => 
             <Accordion>
-              <AccordionSummary aria-controls="panella-content" id={topic}>
+              <AccordionSummary aria-controls="panella-content" id={topic} expandIcon={<ExpandMoreIcon />}>
                 <Typography>{topic} Icebreakers</Typography>
               </AccordionSummary>
               <AccordionDetails>
