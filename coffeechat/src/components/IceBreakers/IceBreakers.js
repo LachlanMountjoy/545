a7407db884b11e0c60636bd6985b923831bd48a1
SHAPS252 <dshap252@gmail.com>
2021-12-02 21:01:33 -0500

    Updated preferences and icebreakers

--- coffeechat/src/components/IceBreakers/IceBreakers.js
+++ coffeechat/src/components/IceBreakers/IceBreakers.js
@@ -1,11 +1,29 @@
 import {icebreakers} from '../../icebreakers.json';
 import NavBar from '../NavBar/NavBar.js';
 
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
+
 function IceBreakers({userObject, setCookie}) {
+  const icebreakerTopics = Object.keys(icebreakers);
     return (
       <div className="IceBreakers">
         <NavBar setCookie={setCookie}/>
-        {icebreakers.map(breaker => <p>{breaker}</p>)}
+        {icebreakerTopics.map(topic => 
+            <Accordion>
+              <AccordionSummary aria-controls="panella-content" id={topic}>
+                <Typography>{topic} Icebreakers</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <ul>
+                  {icebreakers[topic].map(icebreaker => <li>{icebreaker}</li>)}
+                </ul>
+              </AccordionDetails>
+            </Accordion>
+          )}
       </div>
     );
   }
